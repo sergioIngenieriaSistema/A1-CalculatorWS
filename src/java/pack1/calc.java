8c7d1b1f70aa8cd651f5c6ff1e04f96914f806c5
sergio <sldiaz04@hotmail.com>
2018-02-06 17:28:16 +0000

    First commit

---
+++ src/java/pack1/calc.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pack1;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author sergio
+ */
+@WebService(serviceName = "calc")
+@Stateless()
+public class calc {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+
+    /**
+     * Web service operation
+     */
+    @WebMethod(operationName = "sum")
+    public int sum(@WebParam(name = "n1") int n1, @WebParam(name = "n2") int n2) {
+        //TODO write your implementation code here:
+        return n1 + n2;
+    }
+}
